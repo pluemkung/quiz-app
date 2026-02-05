4eba111eaecfa1c5070f10ea507616180320a12b
pluemkung <39697097+pluemkung@users.noreply.github.com>
2026-02-05 09:39:45 +0700

    join server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Menu from "./component/Menu";
+import Quiz from "./component/Quiz";
+import Score from "./component/Score";
 
 function App() {
+  const [ appState,setAppState ] = useState("menu")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Web Development Quiz</h1>
+      { appState === "menu" && <Menu/>}  
+      { appState === "quiz" && <Quiz/>}  
+      { appState === "score" && <Score/>}  
     </div>
   );
 }
---
+++ src/component/Score.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Score = () => {
+  return (
+    <h1>Score Component</h1>
+  )
+}
+
+export default Score
